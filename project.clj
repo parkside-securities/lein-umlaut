bcbcdad0f27caa12762653365dbca942d249f685
Nikolas Moya <moya@work.co>
2017-07-13 22:56:31 +0200

    Bump lein-umlaut

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-umlaut "0.1.1-SNAPSHOT"
+(defproject lein-umlaut "0.1.2-SNAPSHOT"
   :description "CLI for umlaut"
   :url "https://github.com/workco/lein-umlaut"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-future-spec "1.9.0-alpha14"]
-                 [umlaut "0.1.13-SNAPSHOT"]]
+                 [umlaut "0.1.14-SNAPSHOT"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
