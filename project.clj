13bcdb72ccec343f40ebbd34c8bc9ef2c70e704a
Nikolas Moya <moya@work.co>
2017-10-09 19:07:22 +0200

    Bump version and remove SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject lein-umlaut "0.1.3-SNAPSHOT"
+(defproject lein-umlaut "0.1.4"
   :description "CLI for umlaut"
   :url "https://github.com/workco/lein-umlaut"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-future-spec "1.9.0-alpha14"]
-                 [umlaut "0.1.15-SNAPSHOT"]]
+                 [umlaut "0.1.16"]]
+  :deploy-repositories {"clojars" {:sign-releases false}}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
