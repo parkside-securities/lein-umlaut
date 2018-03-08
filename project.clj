a5d1fba586ed8384c394a8099e753a649e7419b5
Nikolas Moya <moya@work.co>
2018-03-08 15:59:51 +0100

    Version bump

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-umlaut "0.5.2"
+(defproject lein-umlaut "0.5.3"
   :description "Leiningen plugin for umlaut"
   :url "https://github.com/workco/lein-umlaut"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [umlaut "0.5.2"]]
+                 [umlaut "0.5.3"]]
   :deploy-repositories {"clojars" {:sign-releases false}}
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
